a0680ae46daa80c1db88478ee23a6f90cec33ba8
petro <mcsemka2@gmail.com>
2025-09-08 23:18:11 -0700

    Initial Commit

---
+++ lab1petro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lab1petro
+//
+//  Created by ІПЗ-31/2 on 08.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("21378 wqeuih")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lab1petro/lab1petroApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab1petroApp.swift
+//  lab1petro
+//
+//  Created by ІПЗ-31/2 on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct lab1petroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
